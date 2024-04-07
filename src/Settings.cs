2c341f66f0d1ea69c790085570f4b88096c36c26
Ondřej Schlaichert <schlaichert.o@gmail.com>
2024-04-07 11:52:30 +0200

    Settings change for production

--- src/Settings.cs
+++ src/Settings.cs
@@ -2,8 +2,8 @@ namespace ApertureMessenger;
 
 public static class Settings
 {
-    // public const string AlmsUrl = "https://alms.oschl.eu/";
-    public const string AlmsUrl = "http://127.0.0.1:5678/"; // for local testing
-    public const string TargetAlmsVersion = "0.0.1";
+    public const string AlmsUrl = "https://alms.oschl.eu/";
+    // public const string AlmsUrl = "http://127.0.0.1:5678/"; // for local testing
+    public const string TargetAlmsVersion = "1.0.0";
     public const int RefreshSleepSeconds = 2;
 }
